f051002615f4c95d5fca84c1a3429d6a893e7859
github-jason <jason.seekjob@gmail.com>
2017-03-10 21:46:42 +1100

    Fri 10 Mar 2017 21:46:47 AEDT rewrite script

--- nsw-lotto.rb
+++ nsw-lotto.rb
@@ -49,65 +49,53 @@ class LottoDraw
 
   def powerball
   # power ball: main number -> six balls, powerball number -> 1 number
-  # first draw the six balls in (1..15), (16..29), (30..45), two numbers in each range,
-  # then draw powerball number in (1..20)
+  # power ball may have awkward result, like this real draw result: 
+  # 25,36,30,9,34,40, p: 11
+  # 19,26,9,29,27,6,  p: 15
+  # 20,29,1,16,15,26, p: 12
+  # so, our splitted arry of numbers is quite interesting
+  # every time, pick up two from the array
+  # generate a number pool, rand pick number from the pool
+    main_number = 6 
     origin_draw_range = [[1, 15], [16, 29], [30, 45]]
-  # get range weight
-    range_weight = rand(2..3)
-    if range_weight == 2
-      draw_range = []
-      draw_range = origin_draw_range.sample(range_weight)
-    powerball_range = [1, 20]
-    main_number = 6
-    each_range_round_for_main = 2
-    drawed_numbers = []
-    draw_range.each do |range|
-      low = range[0]
-      high = range[1]
-      i = 1
-      until i > each_range_round_for_main
-        number = rand(low..high)
-        if !drawed_numbers.include?(number)
-          drawed_numbers += [number]
-          i += 1
-        end
-      end
-    end
-    drawed_numbers.sort! { |x, y| x <=> y }
-    j = 2
-    while j > 1
-      low = powerball_range[0]
-      high = powerball_range[1]
-      powerball_number = rand(low..high)
-      if !drawed_numbers.include?(powerball_number)
-        drawed_numbers += ["powerball number : #{powerball_number}"]
-        j = 0
-      end
-    end
-    return drawed_numbers
+    picked_draw_range = origin_draw_range.sample(2)
+    puts "picked draw range: #{picked_draw_range}" if @debug
+    weight = rand(1..5)
+    puts "weight: #{weight}" if @debug   
+    rand_draw_range = picked_draw_range.sample(1).flatten
+    puts "rand draw range: #{rand_draw_range}" if @debug
+    powerball_main_number = []
+    powerball_main_number += (rand_draw_range[0]..rand_draw_range[1]).to_a.sample(weight)
+    puts "powerball main number: #{powerball_main_number}" if @debug
+    rest_weight = main_number - weight
+    puts "rest_weight: #{rest_weight}" if @debug
+    rest_draw_range = picked_draw_range.flatten - rand_draw_range
+    puts "rest_draw_range: #{rest_draw_range}" if @debug
+    rest_draw_range.sort!
+    puts "rest_draw_range: #{rest_draw_range}" if @debug
+    powerball_main_number += (rest_draw_range[0]..rest_draw_range[1]).to_a.sample(rest_weight)
+    puts "powerball_main_number: #{powerball_main_number}" if @debug
+    powerball_pb_number = ((1..20).to_a - powerball_main_number).sample(1)
+    puts "powerball main number: #{powerball_main_number.sort}, powerball number: #{powerball_pb_number}"
   end
+
+
 
   def satlotto
   # sat lotto: main number -> six balls
   # draw six balls in (1..23), (24..45), three numbers in each range.
     draw_range = [[1, 23], [24, 45]]
     main_number = 6
-    each_range_round_for_main = 3
-    drawed_numbers = []
-    draw_range.each do |range|
-      low = range[0]
-      high = range[1]
-      i = 1
-      until i > each_range_round_for_main
-        number = rand(low..high)
-        if !drawed_numbers.include?(number)
-          drawed_numbers += [number]
-          i += 1
-        end
-      end
-    end
-    drawed_numbers.sort! { |x, y| x <=> y }
-    return drawed_numbers
+    weight = rand(1..5)
+    sample_draw_range = draw_range.sample(1).flatten
+    lotto_number = []
+    lotto_number += (sample_draw_range[0]..sample_draw_range[1]).to_a.sample(weight)
+    puts "lotto_number: #{lotto_number}" if @debug
+    rest_weight = main_number - weight
+    rest_draw_range = (draw_range.flatten - sample_draw_range).sort
+    puts "rest_draw_range: #{rest_draw_range}" if @debug
+    lotto_number += (rest_draw_range[0]..rest_draw_range[1]).to_a.sample(rest_weight)
+    puts "saturday lotto main number: #{lotto_number.sort}"
   end
 
   def ozlotto
@@ -115,57 +103,38 @@ class LottoDraw
   # the draw last number in one range again selected by dice
     draw_range = [[1, 15], [16, 29], [30, 45]]
     main_number = 6
-    each_range_round_for_main = 2
-    drawed_numbers = []
-    draw_range.each do |range|
-      low = range[0]
-      high = range[1]
-      i = 1
-      until i > each_range_round_for_main
-        number = rand(low..high)
-        if !drawed_numbers.include?(number)
-          drawed_numbers += [number]
-          i += 1
-        end
-      end
+    lotto_number = []
+    draw_range.each do |x|
+      lotto_number += (x[0]..x[1]).to_a.sample(2).sort
     end
-    j = 2
-    while j > 1
-      seventh_number = rand(1..45)
-      if !drawed_numbers.include?(seventh_number)
-        drawed_numbers += [seventh_number]
-        j = 0
-      end
-    end
-    drawed_numbers.sort! { |x, y| x <=> y }
-    return drawed_numbers
+    puts "oz lotto main number: #{lotto_number}" if @debug
+    lotto_number += ((1..45).to_a - lotto_number).sample(1)
+    puts "oz lotto main number: #{lotto_number.sort}"
   end
 
   def gettickets
-    drawresult = []
     if @playtype.include?(1)
       puts "This is a Power Ball draw"
       i = 1
       until i > @game
         i += 1
-        drawresult += [powerball]
+        powerball
       end
     elsif @playtype.include?(2)
       puts "This is a Saturday Lotto draw"
       i = 1
       until i > @game
         i += 1
-        drawresult += [satlotto]
+        satlotto
       end
     else
       puts "This is a OZ Lotto draw"
       i = 1
       until i > @game
         i += 1
-        drawresult += [ozlotto]
+        ozlotto
       end
     end
-    return drawresult
   end
 
 end
@@ -174,6 +143,4 @@ end
 draw = LottoDraw.new
 draw.parse_command_line(ARGV)
 results = draw.gettickets
-results.each do |result|
-  puts "one game is picked as :#{result}\n"
-end
+
